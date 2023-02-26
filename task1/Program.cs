1ba8dfc37dece7de68ec3ff73457e80fdf7ad0bb
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-02-26 03:16:51 +0300

    домашнее задание к семинару№4 задача№1 иправление

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,23 +2,59 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int numberA = ReadInt("Число A: ");
-int numberB = ReadInt("Число B: ");
-ToDegree(numberA, numberB);
-
-void ToDegree(int a, int b)
+int Prompt(string message)
 {
-  int result = 1;
-  for (int i = 1; i <= b; i++)
+  System.Console.Write(message);
+  string readInput = System.Console.ReadLine();
+  int result = int.Parse(readInput);
+  return result;
+}
+
+int Power(int powerBase, int exponent)
+{
+  int power = 1;
+  for (int i = 0; i < exponent; i++)
   {
-    result = result * a;
+    power *= powerBase;
   }
-  Console.WriteLine(result);
+  return power;
 }
 
-// Функция ввода
-int ReadInt(string message)
+bool ValidateExponent(int exponent)
 {
-  Console.WriteLine(message);
-  return Convert.ToInt32(Console.ReadLine());
+  if (exponent < 0)
+  {
+    System.Console.WriteLine("Показатель не должен быть меньше нуля");
+    return false;
+  }
+  return true;
 }
+
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+  System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
+
+
+// int numberA = ReadInt("Число A: ");
+// int numberB = ReadInt("Число B: ");
+// ToDegree(numberA, numberB);
+
+// void ToDegree(int a, int b)
+// {
+//   int result = 1;
+//   for (int i = 1; i <= b; i++)
+//   {
+//     result = result * a;
+//   }
+//   Console.WriteLine(result);
+// }
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//   Console.WriteLine(message);
+//   return Convert.ToInt32(Console.ReadLine());
+// }
